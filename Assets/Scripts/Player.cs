3171b1eda81c792109b5e2cf6f9b444b0b651ed3
Eric Freeman <eric.freeman@extendhealth.com>
2014-07-27 21:09:42 -0600

    Added some scripts to move player around and camera track player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        var MaxSpeed = 500;
+        var Acceleration = 25;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if(rigidbody.velocity.x < MaxSpeed)
+                transform.rigidbody.AddForce(Acceleration, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (rigidbody.velocity.x > -MaxSpeed)
+                transform.rigidbody.AddForce(-Acceleration, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/TrackObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TrackObject : MonoBehaviour
+{
+    public Vector3 Offset = Vector3.zero;
+    public Transform ObjectToTrack;
+
+    void Update()
+    {
+        transform.position = ObjectToTrack.position + Offset;
+    }
+}
